2a4a39f914ff0273bea2ef04f45e1d3b5107ffff
Amanda Anderson <aanderson_120@yahoo.com>
2020-11-27 16:22:17 -0600

    changes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const character = document.getElementById ("character");
+const block=document.getElementById("block");
 
+function jump() {
+    console.log(character)
+    character.classList.add("animate");
+   
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
+}
